4d982e018425a19818b1be16f05d5e2c3c1fc98e
田浪 <1584731441@qq.com>
2021-09-17 10:29:12 +0800

    添加dockerfile文件

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 router.post('temp', function (req, res, next) {
-  wxTemplate.sendMsg()
+  wxTemplate.sendMsg({
+    appid: 'wxa62bb7619cace8e5',
+    secret: 'faf956436f7af94cb8ed67031c8a8c92',
+  })
   // res.send('respond with a resource')
 })
 
